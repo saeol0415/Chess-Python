90f945d9aecdd019a869d5cff3efa6556a415238
Saeol Ahn <108802714+saeol0415@users.noreply.github.com>
2025-01-07 20:31:08 +0900

    Update Chess.py

--- Chess.py
+++ Chess.py
@@ -1,39 +1,21 @@
-from array import *
-
 # Creating Empty Board
-board = [[0 for i in range(8)] for j in range(8)]
-
+board = [[0] * 8 for _ in range(8)]
 
 
 # Reset Function
 def reset():
+    global board
+    board = [["" for _ in range(8)] for _ in range(8)]
+    
     for i in range(8):
-        board[i] = ["a"+str(i+1),"b"+str(i+1),"c"+str(i+1),"d"+str(i+1),"e"+str(i+1),"f"+str(i+1),"g"+str(i+1),"h"+str(i+1)]
+        board[i] = [chr(97 + j) + str(i + 1) for j in range(8)]
 
-    board[0][0] = "WR"
-    board[0][1] = "WN"
-    board[0][2] = "WB"
-    board[0][3] = "WQ"
-    board[0][4] = "WK"
-    board[0][5] = "WB"
-    board[0][6] = "WN"
-    board[0][7] = "WR"
-
-    board[7][0] = "BR"
-    board[7][1] = "BN"
-    board[7][2] = "BB"
-    board[7][3] = "BQ"
-    board[7][4] = "BK"
-    board[7][5] = "BB"
-    board[7][6] = "BN"
-    board[7][7] = "BR"
-
+    pieces = ["R", "N", "B", "Q", "K", "B", "N", "R"]
     for i in range(8):
+        board[0][i] = "W" + pieces[i]
+        board[7][i] = "B" + pieces[i]
         board[1][i] = "WP"
-
-    for i in range(8):
         board[6][i] = "BP"
-
 
 
 # Checks if a bishop move is blocked by another piece (True = Blocked, False = Not Blocked)
@@ -214,65 +196,110 @@ def checkP(x1,y1,x2,y2):
     return False
 
 
+# Converts UCI to indices (x, y)
+def indicesFromUCI(algebraic):
+    file = algebraic[0]
+    rank = algebraic[1]
+    x = int(rank) - 1
+    y = ord(file) - 97
+    return x, y
 
-# Defining Move Function
-def move(x1,y1,x2,y2):
-    if board[x1][y1] != chr(y1+97)+str(x1+1):
-        if abs(x1-x2)+abs(y1-y2) != 0:
-            if board[x1][y1][0] != board[x2][y2][0]:
-                if board[x1][y1][1] == "B":
-                    if checkB(x1,y1,x2,y2) == True:
-                        board[x2][y2] = board[x1][y1]
-                        board[x1][y1] = chr(y1+97)+str(x1+1)
-                    else:
-                        print("Bishop at", chr(y1+97)+str(x1+1), "cannot move to", str(chr(y2+97)+str(x2+1)))
-                elif board[x1][y1][1] == "N":
-                    if checkN(x1,y1,x2,y2) == True:
-                        board[x2][y2] = board[x1][y1]
-                        board[x1][y1] = chr(y1+97)+str(x1+1)
-                    else:
-                        print("Knight at", chr(y1+97)+str(x1+1), "cannot move to", str(chr(y2+97)+str(x2+1)))
-                elif board[x1][y1][1] == "R":
-                    if checkR(x1,y1,x2,y2) == True:
-                        board[x2][y2] = board[x1][y1]
-                        board[x1][y1] = chr(y1+97)+str(x1+1)
-                    else:
-                        print("Rook at", chr(y1+97)+str(x1+1), "cannot move to", str(chr(y2+97)+str(x2+1)))
-                elif board[x1][y1][1] == "Q":
-                    if checkQ(x1,y1,x2,y2) == True:
-                        board[x2][y2] = board[x1][y1]
-                        board[x1][y1] = chr(y1+97)+str(x1+1)
-                    else:
-                        print("Queen at", chr(y1+97)+str(x1+1), "cannot move to", str(chr(y2+97)+str(x2+1)))
-                elif board[x1][y1][1] == "K":
-                    if checkK(x1,y1,x2,y2) == True:
-                        board[x2][y2] = board[x1][y1]
-                        board[x1][y1] = chr(y1+97)+str(x1+1)
-                    else:
-                        print("King at", chr(y1+97)+str(x1+1), "cannot move to", str(chr(y2+97)+str(x2+1)))
-                elif board[x1][y1][1] == "P":
-                    if checkP(x1,y1,x2,y2) == True:
-                        board[x2][y2] = board[x1][y1]
-                        board[x1][y1] = chr(y1+97)+str(x1+1)
-                    else:
-                        print("Pawn at", chr(y1+97)+str(x1+1), "cannot move to", str(chr(y2+97)+str(x2+1)))
-            else:
-                print("There is already", board[x2][y2], "in your destination")
-        else:
-            print("You are not moving anything")
-    else:
+# Parses UCI move input into indices (x1, y1, x2, y2)
+def parseUCI(move_input):
+    if len(move_input) != 4:
+        raise ValueError("Invalid move input. Must be in the format 'e2e4'.")
+    start = move_input[:2]
+    end = move_input[2:]
+    x1, y1 = indicesFromUCI(start)
+    x2, y2 = indicesFromUCI(end)
+    return x1, y1, x2, y2
+
+
+# Checks if a move is valid (True = Valid, False = Invalid)
+def isValidMove(x1, y1, x2, y2, turn):
+    piece = board[x1][y1]
+    destination = board[x2][y2]
+
+    if piece == chr(y1 + 97) + str(x1 + 1):
         print("There isn't any piece at", board[x1][y1])
-            
+        return False
 
+    if piece[0] != turn:
+        print(f"It's {turn}'s turn. You can't move {piece}.")
+        return False
 
-# Printing Board Function
+    if abs(x1 - x2) + abs(y1 - y2) == 0:
+        print("You are not moving anything")
+        return False
+
+    if piece[0] == destination[0]:
+        print("There is already", destination, "in your destination")
+        return False
+
+    piece_type = piece[1]
+    if piece_type == "B" and checkB(x1, y1, x2, y2):
+        return True
+    elif piece_type == "N" and checkN(x1, y1, x2, y2):
+        return True
+    elif piece_type == "R" and checkR(x1, y1, x2, y2):
+        return True
+    elif piece_type == "Q" and checkQ(x1, y1, x2, y2):
+        return True
+    elif piece_type == "K" and checkK(x1, y1, x2, y2):
+        return True
+    elif piece_type == "P" and checkP(x1, y1, x2, y2):
+        return True
+    else:
+        print(f"{piece_type} at", chr(y1 + 97) + str(x1 + 1), "cannot move to", chr(y2 + 97) + str(x2 + 1))
+        return False
+
+# UCI Format Move Function
+def moveUCI(move_input, turn):
+    try:
+        x1, y1, x2, y2 = parseUCI(move_input)
+        if isValidMove(x1, y1, x2, y2, turn):
+            movePiece(x1, y1, x2, y2)
+            return True
+        return False
+    except ValueError as e:
+        print(e)
+        return False
+
+# Move Piece Function
+def movePiece(x1, y1, x2, y2):
+    board[x2][y2] = board[x1][y1]
+    board[x1][y1] = chr(y1 + 97) + str(x1 + 1)
+
+# Printing Board Function with Unicode Characters and Outlines
 def drawBoard():
+    piece_symbols = {
+        "WK": "♔", "WQ": "♕", "WR": "♖", "WB": "♗", "WN": "♘", "WP": "♙",
+        "BK": "♚", "BQ": "♛", "BR": "♜", "BB": "♝", "BN": "♞", "BP": "♟"
+    }
+    print("  a b c d e f g h")
+    print(" +----------------+")
     for i in range(8):
+        print(f"{8-i}|", end="")
         for j in range(8):
-            print(board[7-i][j], end = ' ')
-        print()
+            piece = board[7-i][j]
+            if piece in piece_symbols:
+                print(piece_symbols[piece], end="")
+            else:
+                print(" ", end=" ")
+        print(f"|{8-i}")
+    print(" +----------------+")
+    print("  a b c d e f g h")
 
+# Main Game Loop
+def main():
+    reset()
+    drawBoard()
+    turn = "W"
+    while True:
+        move_input = input(f"Enter move for {'White' if turn == 'W' else 'Black'}: ")
+        if moveUCI(move_input, turn):
+            drawBoard()
+            turn = "B" if turn == "W" else "W"
 
-        
-reset()
-drawBoard()
+if __name__ == "__main__":
+    main()
